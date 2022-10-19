2c08b3660f7021e9992c4a62663ff64c177d25cf
Mercedes Bta <mindrecole@gmail.com>
2022-10-19 14:58:27 +0200

    primer commit

---
+++ src/main/java/com/example/primerartefacto/PrimerArtefactoApplication.java
@@ -0,0 +1,13 @@
+package com.example.primerartefacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerArtefactoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerArtefactoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/primerartefacto/PrimerArtefactoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primerartefacto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerArtefactoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
